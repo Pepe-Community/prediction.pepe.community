3b889dbc70ef2e036b421c287982347231e6ff0c
Tam Nguyen <nntam2013@gmail.com>
2021-07-31 17:03:27 +0700

    chore: update interval

--- src/views/Predictions/components/Menu.tsx
+++ src/views/Predictions/components/Menu.tsx
@@ -54,7 +54,7 @@ const Menu = () => {
       <SetCol>
         <Flex alignItems="center" justifyContent="flex-end">
           <TimerLabelWrapper>
-            <TimerLabel interval="5" unit="m" />
+            <TimerLabel interval="10" unit="m" />
           </TimerLabelWrapper>
           <HelpButtonWrapper>
             <IconButton
